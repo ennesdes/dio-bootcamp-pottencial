8cf12ca44cdac4bf06edbd822dd62d56a5dbe9f7
Douglas Ennes <92762450+ennesdes@users.noreply.github.com>
2022-09-19 16:08:09 -0300

    Projeto calculadora
    Operações aritméticas e classe math

---
+++ Projetos/OperadoresAritmeticos/Program.cs
@@ -0,0 +1,14 @@
+﻿using OperadoresAritmeticos;
+
+Calculadora calculadora = new Calculadora();
+
+calculadora.Somar(10, 10);
+calculadora.Subtrair(10, 10);
+calculadora.Multiplicar(10, 10);
+calculadora.Dividir(10, 10);
+calculadora.Potencia(10, 10);
+calculadora.Seno(10);
+calculadora.Coseno(10);
+calculadora.Tangente(10);
+calculadora.Raiz(10);
+
